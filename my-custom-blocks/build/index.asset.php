3575eb77c8cc79a00f663e30cd75e56b00c77052
Ryan <ryansimms4@hotmail.com>
2025-03-13 13:35:47 +0000

    CSS Updates

--- my-custom-blocks/build/index.asset.php
+++ my-custom-blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '6a39b2ad66893e39f6f1');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '598e2ec9f0b36751bc12');
