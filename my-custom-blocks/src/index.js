5232f329208c534aaf000b5cbf276ae4a3da91c5
Ryan <ryansimms4@hotmail.com>
2025-03-13 12:45:49 +0000

    Update index.js

--- my-custom-blocks/src/index.js
+++ my-custom-blocks/src/index.js
@@ -1,4 +1,6 @@
 // Import each block
 import './blocks/bar-chart/bar-chart.js';
+import './blocks/bar-chart/bar-animation.js';
 import './blocks/new-block/new-block.js';
 import './blocks/slider/slider.js';
+import './blocks/slider/slider-navigation.js';
