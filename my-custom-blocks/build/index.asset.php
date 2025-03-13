79f55158b8df247f60a2eb6e6b4895ef890d8f6f
Ryan <ryansimms4@hotmail.com>
2025-03-13 15:45:31 +0000

    Build

--- my-custom-blocks/build/index.asset.php
+++ my-custom-blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '806eb865b384dd12f1f8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '3e6745486843817b6215');
