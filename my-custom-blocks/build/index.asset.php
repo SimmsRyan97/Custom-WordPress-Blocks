7be32220da9e21a8cc4fefb62201eee263ad1e0f
Ryan <ryansimms4@hotmail.com>
2025-06-13 14:08:03 +0100

    Editor update

--- my-custom-blocks/build/index.asset.php
+++ my-custom-blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '384752b4cbe329c4cd90');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e8a9fe28634b487a3f0e');
