d376c225af35ef739612b0602f9ecc3dbb05123a
Ryan <ryansimms4@hotmail.com>
2025-06-13 14:13:52 +0100

    Styles

--- my-custom-blocks/build/index.asset.php
+++ my-custom-blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e8a9fe28634b487a3f0e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f1d750ea7554c57f0276');
